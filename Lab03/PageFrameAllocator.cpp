3eb9f16ea0aead90e5f075753f7223b4327f2b69
maxwellthefourth <maxwellthefourth@maxwellthefourth-VirtualBox>
2018-01-29 18:41:06 -0700

    random bits

--- Lab03/PageFrameAllocator.cpp
+++ Lab03/PageFrameAllocator.cpp
@@ -6,22 +6,32 @@
  */
 
 #include <vector>
-
 #include "PageFrameAllocator.h"
 
 PageFrameAllocator::PageFrameAllocator(int numPageFrames) {
     int totalSize = numPageFrames * 0x1000;
-    PageFrameAllocator::memory.resize(totalSize); //Resizes memory
+    memory.resize(totalSize, 0); //Resizes memory
     
-    PageFrameAllocator::freeListHead = 0xFFFFFFFF //Building free list
-    PageFrameAllocator::pageFramesFree = totalSize;  
-    PageFrameAllocator::pageFramesTotal = totalSize;
+    freeListHead = 0xFFFFFFFF; // The page frame number of the first page frame in the free list (0xFFFFFFFF if list empty)
+    uint32_t index, v32;
+    for (int i = 0; i < numPageFrames; i++) { // Build free list
+        freeListHead = 0;
+        index = i*0x1000;
+        v32 = i+1;
+        if (i == numPageFrames-1) {
+            v32 = 0xFFFFFFFF; // Last element of the list so 0xFFFFFFFF
+        }
+        memcpy(&memory[index], &v32, sizeof(uint32_t));
+    }
+    
+    pageFramesFree = numPageFrames;  
+    pageFramesTotal = numPageFrames;
 }
 
-PageFrameAllocator::PageFrameAllocator(const PageFrameAllocator& orig) {
-}
-
-PageFrameAllocator::~PageFrameAllocator() {
+bool PageFrameAllocator::Allocate(uint32_t count, std::vector<uint32_t> &page_frames) {
     
 }
 
+bool PageFrameAllocator::Deallocate(uint32_t count, std::vector<uint32_t> &page_frames) {
+    
+}
